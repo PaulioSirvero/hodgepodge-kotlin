0e4165a8c5c3d6c46d5c9b56453d4d2b5a1eb29d
PaulioRandall <PaulioSirvero@users.noreply.github.com>
2019-02-23 17:34:50 +0000

    Update 'HttpError.kt'

--- src/main/kotlin/HttpError.kt
+++ src/main/kotlin/HttpError.kt
@@ -1,137 +1,111 @@
 
-/**
+/******************************************************************************
  * Thrown when a HTTP service encounters an error
  *
  * @property[status] HTTP status code
- * @property[msg] Error message
+ * @property[publicMsg] Error message
  * @param[cause] Cause of the error
- */
-class HttpError(
+ *****************************************************************************/
+open class HttpError(
   val status: Int,
-  val msg: String,
+  val publicMsg: String,
   cause: Exception? = null
-): Exception(msg, cause) {
+): Exception(publicMsg, cause) {
 
   init {
     when {
-      status < 400 -> throw HttpError.bug(
+      status < 400 -> throw bug(
         "HTTP error status codes must be 400 or greater")
-      status >= 600 -> throw HttpError.bug(
+      status >= 600 -> throw bug(
         "HTTP error status codes must be less than 600")
     }
   }
 
-  /**
-   * Represents a Lean reply for an erroneous call to an endpoint.
-   * It is serialised into the target format directly
-   *
-   * @property[message] Human readable message describing the response
-   * @property[lean] Version of the lean format
-   */
-  data class ErrorReply(
-    val message: String
-  ) {
-    val lean: Int = 1
-  }
-
-  /**
-   * Converts the exception into an object that can be sent
-   * back to the client
-   */
-  fun toReply() = ErrorReply(
-    when (status) {
-      500 -> DEFAULT_ERROR_MESSAGE
-      else -> msg
-    }
-  )
-
   companion object {
 
-    /** Default service error message for when one is not provided */
-    const val DEFAULT_ERROR_MESSAGE = "Internal service error"
-
-    /**
-     * Returns a client error with a 400 status when a
-     * client supplied parameter is missing or invalid
+    /**************************************************************************
+     * Returns a client error with a 400 status when a client supplied
+     * parameter is missing or invalid
      *
-     * @param[msg] Response message
-     */
-    fun badRequest(msg: String) = HttpError(400, msg)
+     * @param[publicMsg] Public response message
+     *************************************************************************/
+    fun badRequest(publicMsg: String) = HttpError(400, publicMsg)
 
-    /**
-     * Returns a client error with a 401 status when a
-     * client attempts to access a resource before they
-     * have authenticated or there authentication details
-     * were invalid
+    /**************************************************************************
+     * Returns a client error with a 401 status when a client attempts to
+     * access a resource before they have authenticated or there authentication
+     * details were invalid
      *
-     * @param[msg] Response message
-     */
-    fun badLogin(msg: String) = HttpError(401, msg)
+     * @param[publicMsg] Public response message
+     *************************************************************************/
+    fun badLogin(publicMsg: String) = HttpError(401, publicMsg)
 
-    /**
-     * Returns a client error with a 403 status when a
-     * client attempts to access a resource they are not
-     * authorised to access
+    /**************************************************************************
+     * Returns a client error with a 403 status when a client attempts to
+     * access a resource they are not authorised to access
      *
-     * @param[msg] Response message
-     */
-    fun notAllowed(msg: String) = HttpError(403, msg)
+     * @param[publicMsg] Public response message
+     *************************************************************************/
+    fun notAllowed(publicMsg: String) = HttpError(403, publicMsg)
 
-    /**
+    /**************************************************************************
      * Returns a client error with a 404 'not found' status
      *
-     * @param[msg] Custom response message
-     */
-    fun notFound(msg: String = "No such resource") = HttpError(404, msg)
+     * @param[publicMsg] Public response message
+     *************************************************************************/
+    fun notFound(
+      publicMsg: String = "No such resource"
+    ) = HttpError(404, publicMsg)
 
-    /**
-     * Returns a service error with a 500 status when a bug
-     * is detected
+    /**************************************************************************
+     * Returns a service error with a 500 status when a bug is detected
      *
-     * @param[msg] Response message
+     * @param[internalMsg] Message for internal use, logs etc..
      * @param[cause] Cause of the error
-     */
+     *************************************************************************/
     fun bug(
-      msg: String,
+      internalMsg: String,
       cause: Exception? = null
-    ) = HttpError(500, msg, cause)
+    ) = HttpError(
+      500,
+      "Internal service error",
+      Exception(internalMsg, cause)
+    )
 
-    /**
-     * Returns a service error with a 500 status when a bad
-     * service configuration is detected
+    /**************************************************************************
+     * Returns a service error with a 500 status when a bad service
+     * configuration is detected
      *
-     * @param[msg] Response message
+     * @param[publicMsg] Public response message
      * @param[cause] Cause of the error
-     */
+     *************************************************************************/
     fun badConfig(
-      msg: String,
+      publicMsg: String,
       cause: Exception? = null
-    ) = HttpError(500, msg, cause)
+    ) = HttpError(500, publicMsg, cause)
 
-    /**
-     * Returns a service error with a 501 status when a
-     * service feature has been declared but not
-     * implemented
+    /**************************************************************************
+     * Returns a service error with a 501 status when a service feature has
+     * been declared but not implemented
      *
-     * @param[msg] Response message
+     * @param[publicMsg] Public response message
      * @param[cause] Cause of the error
-     */
+     *************************************************************************/
     fun notImplemented(
-      msg: String,
+      publicMsg: String,
       cause: Exception? = null
-    ) = HttpError(501, msg, cause)
+    ) = HttpError(501, publicMsg, cause)
 
-    /**
-     * Returns a service error with a 503 status when a
-     * service feature is unavailable due to a known
-     * issue or maintenance
+    /**************************************************************************
+     * Returns a service error with a 503 status when a service feature is
+     * unavailable due to a known issue or maintenance
      *
-     * @param[msg] Response message
+     * @param[publicMsg] Public response message
      * @param[cause] Cause of the error
-     */
+     *************************************************************************/
     fun featureUnavailable(
-      msg: String,
+      publicMsg: String,
       cause: Exception? = null
-    ) = HttpError(503, msg, cause)
+    ) = HttpError(503, publicMsg, cause)
   }
 }
